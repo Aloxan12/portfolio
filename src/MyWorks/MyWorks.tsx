b7a5358ff1a78150b08508b62a11be0dc1a0a963
Aloxan12 <kvakvaaak@mail.ru>
2021-05-11 13:24:00 +0300

    add font-family for title

--- src/MyWorks/MyWorks.tsx
+++ src/MyWorks/MyWorks.tsx
@@ -7,7 +7,9 @@ function MyWorks() {
     return (
         <div className={style.myWorksBlock}>
             <div className={`${styleContainer.container} ${style.myWorksContainer}`}>
-                <h3 className={style.title}>My Works</h3>
+                <div className={style.title}>
+                    <h2>My Works</h2>
+                </div>
                 <div className={style.works}>
                     <MyWork title={'React'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit'}/>
                     <MyWork title={'JS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit'}/>

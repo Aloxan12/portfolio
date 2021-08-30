5b83701c31b2a4422512364f55ea77e09b267b5f
Aloxan12 <kvakvaaak@mail.ru>
2021-08-30 15:49:54 +0300

    style

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from './../Common/styles/Container.module.css';
+import myPhoto from '../assets/image/photo_2020-02-09_17-19-28.jpg'
 
 
 function Main() {
     return (
         <div className={style.mainBlock}>
-            <div className={styleContainer.container}>
+            <div className={`${styleContainer.container} + '' + ${style.container}`}>
                 <div className={style.text}>
-                    <span>Hi There</span>
+                    <span>Welcome</span>
                     <h1>I'm Alexey Shavel</h1>
                     <p>Frontend Developer</p>
                 </div>
-                <div className={style.photo}></div>
+                <div className={style.photoContainer}>
+                    <img className={style.photo} src={myPhoto}/>
+                </div>
             </div>
         </div>
     )

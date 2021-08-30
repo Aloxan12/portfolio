0fe23e1aa242ee9a3070d7d8861035d1a0701913
Aloxan12 <kvakvaaak@mail.ru>
2021-08-30 12:38:59 +0300

    test

--- src/Skills/Skill/Skill.tsx
+++ src/Skills/Skill/Skill.tsx
@@ -10,7 +10,7 @@ export type SkillType = {
 function Skill(props: SkillType) {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
+            <div className={style.icon}>D</div>
             <h3 className={style.title}>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
         </div>
